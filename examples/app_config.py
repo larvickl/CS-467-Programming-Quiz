93dac092db4a6cefd8aaadfd59f65f179424e61d
aneugart <aneugart.dev@gmail.com>
2024-10-06 20:51:13 -1000

    Added example deployment to examples directory.

---
+++ examples/app_config.py
@@ -0,0 +1,31 @@
+import os
+import datetime as dt
+from app_secrets.secret_key import SECRET_KEY
+
+deployment_dir = os.path.abspath(os.path.dirname(__file__))  # Directory containing this file.
+
+class App_Config:
+    # Flask Config
+    DEBUG = False
+    SECRET_KEY = SECRET_KEY
+    SESSION_COOKIE_NAME = "programming_quiz_session_cookie"
+    SESSION_COOKIE_DOMAIN = None
+    SESSION_COOKIE_PATH = None
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "Strict"
+    PERMANENT_SESSION_LIFETIME = dt.timedelta(days=2)
+    SESSION_REFRESH_EACH_REQUEST = False
+    SEND_FILE_MAX_AGE_DEFAULT = None
+    SERVER_NAME = "127.0.0.1:5000"
+    APPLICATION_ROOT = "/"
+    PREFERRED_URL_SCHEME = "https"
+    MAX_CONTENT_LENGTH = None
+    MAX_COOKIE_SIZE = 4093
+
+    # Logging
+    APP_LOG_ENABLED = True
+    APP_LOG_DIR = os.path.join(deployment_dir, "logs")
+    APP_LOG_FILE_NAME = "programming_quiz.log"
+    APP_LOG_FILE_MAX_BYTES = 51200  # 50KiB
+    APP_LOG_FILE_BACKUP_COUNT = 10
---
+++ examples/wsgi.py
@@ -0,0 +1,4 @@
+from programming_quiz_web_app import create_app
+from app_config import App_Config
+
+application = create_app(app_config=App_Config)
